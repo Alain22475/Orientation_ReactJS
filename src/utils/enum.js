21083b3de2a2bf67894bb5d4fe646b5025c869e4
tifan9 <uwasesophie91@gmail.com>
2025-02-18 22:27:02 +0200

    final

---
+++ src/utils/enum.js
@@ -0,0 +1,28 @@
+
+export const backend_path={
+
+    // STUDENT SECTION
+    LOGIN:'',
+    REGISTER:'student/register',
+    DISPLAY_STUDENTS:'student/all',
+    STUDENT_ID:'student/search_student/',
+    UPDATE_STUDENT:'student/updateStudent/',
+    DELETE_STUDENT:'student/deleteStudent/',
+
+    // PROGRESS SECTION
+    PROGRESS_STUDENT_ID:'orientation/student/',
+    SAVE_PROGRESS:'orientation/save_progress',
+    ALL_PROGRESS:'orientation/all_progress',
+    UPDATE_PROGRESS:'orientation/update/',
+    DELETE_PROGRESS:'orientation/delete/',
+
+    //EVENT SECTION
+    ALL_EVENTS:'events/all_events',
+    SEARCH_EVENT:'events/all_events/',
+    CREATE_EVENT:'events/create_events',
+    UPDATE_EVENT:'events/update/',
+    DELETE_EVENT:'events/delete/',
+
+
+
+}
